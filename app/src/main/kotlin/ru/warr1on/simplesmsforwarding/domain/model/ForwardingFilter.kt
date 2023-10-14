27b3765793da7375c7891a7a67638243adab38a1
warr1on <elf84lvl@gmail.com>
2023-10-15 03:20:49 +0700

    Added an ignoreCase param and a commentary for ForwardingFilter

--- app/src/main/kotlin/ru/warr1on/simplesmsforwarding/domain/model/ForwardingFilter.kt
+++ app/src/main/kotlin/ru/warr1on/simplesmsforwarding/domain/model/ForwardingFilter.kt
@@ -3,16 +3,25 @@ package ru.warr1on.simplesmsforwarding.domain.model
 enum class FilterType {
     INCLUDE, EXCLUDE
 }
+
+/**
+ * A filter to determine if the specific message should be forwarded or not.
+ *
+ * @param filterType The type of the filter, i.e. inclusion or exclusion
+ * @param text A text value which should contain key word or phrase to be used as a filter
+ * @param ignoreCase An optional flag that determines if the original text case should be respected
+ */
 data class ForwardingFilter(
     val filterType: FilterType,
-    val text: String
+    val text: String,
+    val ignoreCase: Boolean = false
 ) {
     fun checkIfMessagePassesThisFilter(message: SmsMessage): Boolean {
         val messageText = message.body
         return if (filterType == FilterType.INCLUDE) {
-            messageText.contains(text)
+            messageText.contains(text, ignoreCase)
         } else {
-            !messageText.contains(text)
+            !messageText.contains(text, ignoreCase)
         }
     }
 }
