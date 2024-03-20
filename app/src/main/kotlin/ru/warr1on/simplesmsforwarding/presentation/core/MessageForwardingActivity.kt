d88429d1842a634de788ca3e090590f0f29a2b0c
Warr1on <elf84lvl@gmail.com>
2024-03-20 20:04:35 +0700

    Added a KoinAndroidContext composable wrapper into the root composable to later use Koin in the Compose code

--- app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/MessageForwardingActivity.kt
+++ app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/MessageForwardingActivity.kt
@@ -4,19 +4,24 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.compose.setContent
 import androidx.core.view.WindowCompat
+import org.koin.androidx.compose.KoinAndroidContext
+import org.koin.core.annotation.KoinExperimentalAPI
 import ru.warr1on.simplesmsforwarding.presentation.core.components.modal.ModalHostExperimental
 import ru.warr1on.simplesmsforwarding.presentation.core.navigation.NavigationContainer
 import ru.warr1on.simplesmsforwarding.presentation.core.theme.AppTheme
 
 class MessageForwardingActivity : AppCompatActivity() {
 
+    @OptIn(KoinExperimentalAPI::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            AppTheme {
-                ModalHostExperimental {
-                    NavigationContainer()
+            KoinAndroidContext {
+                AppTheme {
+                    ModalHostExperimental {
+                        NavigationContainer()
+                    }
                 }
             }
         }
