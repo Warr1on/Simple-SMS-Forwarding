39f4555c96eec543f1b8c02fdc4bc93c39cf48b4
Warr1on <elf84lvl@gmail.com>
2024-03-20 20:09:49 +0700

    Reorganized the code in ForwardingMainScreen.kt in preparation of writing an actual fully working implementation of the screen instead of a simple mockup

--- app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/forwardingMainScreen/ui/ForwardingMainScreen.kt
+++ app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/forwardingMainScreen/ui/ForwardingMainScreen.kt
@@ -8,22 +8,56 @@ import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.simplemobiletools.smsmessenger.R
+import org.koin.androidx.compose.koinViewModel
+import ru.warr1on.simplesmsforwarding.presentation.core.extensions.verticalSpacer
 import ru.warr1on.simplesmsforwarding.presentation.core.theme.AppTheme
+import ru.warr1on.simplesmsforwarding.presentation.forwardingMainScreen.ForwardingMainScreenViewModel
 import ru.warr1on.simplesmsforwarding.presentation.forwardingMainScreen.ui.components.ForwardableMessageView
 
-@OptIn(ExperimentalMaterial3Api::class)
+/**
+ * Main forwarder screen composable
+ */
 @Composable
 fun MessageForwardingScreen(
+    viewModel: ForwardingMainScreenViewModel = koinViewModel(),
     onNavigateToSettings: () -> Unit
 ) {
+    ForwardingScreenMainLayout(
+        onNavigateToSettings = onNavigateToSettings
+    )
+}
 
+/**
+ * A composed full layout of the main forwarder screen
+ */
+@Composable
+private fun ForwardingScreenMainLayout(
+    onNavigateToSettings: () -> Unit
+) {
+    ForwardingScreenScaffold(onNavigateToSettings = onNavigateToSettings) { paddingValues ->
+        ForwardingRecordsListView(
+            topPadding = 12.dp + paddingValues.calculateTopPadding(),
+            bottomPadding = 12.dp + paddingValues.calculateBottomPadding()
+        )
+    }
+}
+
+/**
+ * A scaffold with the top app bar for the main forwarder screen
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun ForwardingScreenScaffold(
+    onNavigateToSettings: () -> Unit,
+    content: @Composable (PaddingValues) -> Unit
+) {
     val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior()
 
     Scaffold(
@@ -62,24 +96,31 @@ fun MessageForwardingScreen(
             )
         }
     ) { paddingValues ->
-        LazyColumn {
-            item {
-                Spacer(modifier = Modifier.height(12.dp + paddingValues.calculateTopPadding()))
-            }
-            items(count = 10) {
-                ForwardableMessageView()
-            }
-            item {
-                Spacer(modifier = Modifier.height(12.dp + paddingValues.calculateBottomPadding()))
-            }
-        }
+        content(paddingValues)
     }
 }
+
+@Composable
+private fun ForwardingRecordsListView(
+    topPadding: Dp,
+    bottomPadding: Dp
+) {
+    LazyColumn {
+        verticalSpacer(topPadding)
+        items(count = 10) {
+            ForwardableMessageView()
+        }
+        verticalSpacer(bottomPadding)
+    }
+}
+
+
+// --- Previews ---//
 
 @Preview
 @Composable
 private fun MessageForwardingScreenPreview() {
     AppTheme {
-        MessageForwardingScreen {}
+        ForwardingScreenMainLayout {}
     }
 }
