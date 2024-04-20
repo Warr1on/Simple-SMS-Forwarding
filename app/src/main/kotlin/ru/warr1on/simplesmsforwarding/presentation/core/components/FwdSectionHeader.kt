e443fca0f038462dc151f6d36de6b81ab49c2c06
Warr1on <elf84lvl@gmail.com>
2024-04-20 19:24:53 +0700

    Added an option to set custom padding through modifier and added a doc comment for FwdSectionHeader

--- app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/components/FwdSectionHeader.kt
+++ app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/components/FwdSectionHeader.kt
@@ -13,17 +13,30 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.warr1on.simplesmsforwarding.presentation.core.theme.AppTheme
 
+/**
+ * Default section header for the forwarder app
+ *
+ * @param title Section title
+ * @param useDefaultHeaderPadding Determines if the section header should have default padding added to it
+ * @param modifier Modifier for the section header
+ */
 @Composable
 fun FwdSectionHeader(
     title: String,
+    useDefaultHeaderPadding: Boolean = true,
     modifier: Modifier = Modifier
 ) {
+    val headerModifier = when (useDefaultHeaderPadding) {
+        true -> modifier.padding(start = 12.dp)
+        false -> modifier
+    }
+
     Text(
         text = title,
         fontSize = 18.sp,
         fontWeight = FontWeight.Light,
         color = MaterialTheme.colorScheme.primary,
-        modifier = modifier.padding(start = 12.dp)
+        modifier = headerModifier
     )
 }
 
