9088ae9940c76eef715d08a4c23f31339540c9ea
Warr1on <elf84lvl@gmail.com>
2024-04-20 19:16:33 +0700

    Added some additional settings and doc comments for the FwdDefaultPresetCard

--- app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/components/FwdDefaultPresetCard.kt
+++ app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/components/FwdDefaultPresetCard.kt
@@ -10,18 +10,42 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ru.warr1on.simplesmsforwarding.presentation.core.theme.AppTheme
 
+/**
+ * A general-purpose card container with the default forwarder app preset.
+ *
+ * @param modifier The card container's modifier
+ * @param shape Card shape
+ * @param elevation Card elevation. Determines the shadow and surface color intensity
+ * @param disableShadow Determines if the card should have a shadow. If false, the shadow
+ * would be drawn normally; if true, the card container would be clipped to its shape, and
+ * the outer shadow wouldn't be visible because of that. False by default.
+ * @param useOutlineForDarkTheme Determines if the card should have an outline in the dark mode.
+ * True by default.
+ * @param contentPadding The padding between the card container and its contents
+ * @param content The card's content
+ */
 @Composable
 fun FwdDefaultPresetCard(
     modifier: Modifier = Modifier,
+    shape: Shape = RoundedCornerShape(12.dp),
+    elevation: Dp = 2.dp,
+    disableShadow: Boolean = false,
+    useOutlineForDarkTheme: Boolean = true,
     contentPadding: PaddingValues = PaddingValues(0.dp),
     content: @Composable () -> Unit
 ) {
     val cardBorder: BorderStroke? = if (isSystemInDarkTheme()) {
-        BorderStroke(1.dp, MaterialTheme.colorScheme.secondary)
+        when (useOutlineForDarkTheme) {
+            true -> BorderStroke(1.dp, MaterialTheme.colorScheme.secondary)
+            false -> null
+        }
     } else {
         null
     }
@@ -40,7 +64,7 @@ fun FwdDefaultPresetCard(
 
     val cardElevation: CardElevation = if (!isSystemInDarkTheme()) {
         CardDefaults.cardElevation(
-            defaultElevation = 2.dp
+            defaultElevation = elevation
         )
     } else {
         CardDefaults.cardElevation(
@@ -48,12 +72,17 @@ fun FwdDefaultPresetCard(
         )
     }
 
+    val cardModifier = when (disableShadow) {
+        false -> modifier
+        true -> modifier.clip(shape)
+    }
+
     Card(
-        shape = RoundedCornerShape(12.dp),
+        shape = shape,
         border = cardBorder,
         colors = cardColors,
         elevation = cardElevation,
-        modifier = modifier
+        modifier = cardModifier
     ) {
         Box(modifier = Modifier.padding(contentPadding)) {
             content()
