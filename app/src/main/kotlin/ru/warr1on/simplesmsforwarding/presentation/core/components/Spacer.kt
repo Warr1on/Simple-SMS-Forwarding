d819546710e1bfa2184a2dd66cd8d8ef35e73573
Warr1on <elf84lvl@gmail.com>
2024-04-23 20:57:04 +0700

    Rolled back on some changes in Spacer.kt regarding removal of the layout weight modifiers as it broke the desired spacer layout behavior >_<

--- app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/components/Spacer.kt
+++ app/src/main/kotlin/ru/warr1on/simplesmsforwarding/presentation/core/components/Spacer.kt
@@ -55,34 +55,54 @@ fun HorizontalSpacer(
  * Creates a vertical spacer.
  *
  * You can either specify the exact [height] of the spacer, or leave the field empty,
- * in which case the spacer will try to take up as much available space as it can
+ * in which case the spacer will try to take up as much available space as it can, but
+ * without encroaching on the other composables due to having low default layout weight
+ * of 0.1 (which can be overridden by providing a [weight] param)
  *
  * @param height Height of the spacer in DP. If the height is not specified,
  * the spacer would try to take up all available space.
+ * @param weight Layout weight of the spacer, 0.1 by default. Has no effect if the [height]
+ * value is provided
  * @param modifier An optional modifier for the spacer
  */
 @Composable
 fun ColumnScope.Spacer(
     height: Dp? = null,
+    weight: Float = 0.1f,
     modifier: Modifier = Modifier
 ) {
-    VerticalSpacer(height, modifier)
+    val spacerModifier = when (height) {
+        null -> modifier.fillMaxHeight().weight(weight)
+        else -> modifier.height(height)
+    }
+
+    Spacer(spacerModifier)
 }
 
 /**
  * Creates a horizontal spacer.
  *
  * You can either specify the exact [width] of the spacer, or leave the field empty,
- * in which case the spacer will try to take up as much available space as it can
+ * in which case the spacer will try to take up as much available space as it can, but
+ * without encroaching on the other composables due to having low default layout weight
+ * of 0.1 (which can be overridden by providing a [weight] param)
  *
  * @param width Width of the spacer in DP. If the width is not specified,
  * the spacer would try to take up all available space
+ * @param weight Layout weight of the spacer, 0.1 by default. Has no effect if the [width]
+ * value is provided
  * @param modifier An optional modifier for the spacer
  */
 @Composable
 fun RowScope.Spacer(
     width: Dp? = null,
+    weight: Float = 0.1f,
     modifier: Modifier = Modifier
 ) {
-    HorizontalSpacer(width, modifier)
+    val spacerModifier = when (width) {
+        null -> modifier.fillMaxWidth().weight(weight)
+        else -> modifier.width(width)
+    }
+
+    Spacer(spacerModifier)
 }
